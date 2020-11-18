80b3f12c84cd9ff4e81f3d9f0a3ca8717e86b8ed
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-11-18 15:03:40 +0600

    2) Variables
    This file is all about how to declare variables 
    Mostly all about variable

---
+++ 2_variables.py
@@ -0,0 +1,13 @@
+# price = 10
+# ratings = 4.9
+# print(ratings)
+# name = 'Utsho'
+# is_published = False
+#
+# full_name = 'john Smith'
+# age = 20
+# is_new = True
+
+name = input('What is your name? ')
+favourite_color = input('What is your favourite color? ')
+print(name + ' likes '+ favourite_color )
