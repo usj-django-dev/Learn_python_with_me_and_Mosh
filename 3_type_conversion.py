c759fcccbc1175b4b7a64cbc6dd4ef915890b073
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2020-11-19 07:12:27 +0600

    3) Change variable type
    Change different types of variable

---
+++ 3_type_conversion.py
@@ -0,0 +1,61 @@
+# un_comment line 20 -- 24 run the programme read line 4--15 & comment line 20--24
+# un_comment line 36 -- 38 run the programme read line 31--34 & comment line 36--38
+
+# birth_year = 1998 ( this is a n example of an integer )
+# birth_year = '1998' ( this is a n example of a string )
+# inside of a input function you always get a string,
+# you should convert it to a integer or a float, if you want a neumerical value
+
+
+# what we will do:
+# take a birth_year from user inside input function which is always a string value
+# check the type of birth_year which is string cause of input function
+# calculating the age: this year - birth_year. make birth_year a integer value with int function
+# check the type of age which is int type
+# print age
+
+# 1st programme
+
+
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2020 - int(birth_year)
+# print(type(age))
+# print(age)
+
+##########################################################
+##########################################################
+##########################################################
+##########################################################
+
+# 2nd programme
+# here again we take weight_lbs variable inside input function which is a string
+# inorder to multiply this string with 0.45 we make this string integer through input function
+# print weight in kg
+
+# weight_lbs = input('weight (lbs): ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
