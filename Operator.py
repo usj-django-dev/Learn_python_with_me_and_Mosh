38e3cffdcaec6261bfa8afcad0c091214493e354
Utsho Sadhak Joy <usj.django.dev@gmail.com>
2021-01-27 13:18:51 +0600

    5__Operator.py
    Arithmetic operator  & operator precident

---
+++ Operator.py
@@ -0,0 +1,34 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print(10+3)
+print(10-3)
+print(10*3)
+print(10**3)
+print(10/3)
+print(10//3)
+print(10%3)
+x = 10
+x = x + 3
+print(x)
+
+A = 10
+A +=3 
+A -=3 
+A *=3 
+A /=3 
+A //=3 
+A %=3 
+print(A)
+# argumented assignment operator
+
+
+G = 10+ 3*2
+print(G)
+
+G = 10+ 3*2**2
+print(G)
+G = (10+3)*2**2
+print(G)
+G = (2+3)*10-3
+print(G)
+
